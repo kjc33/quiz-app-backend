ce8b46e8f378412602dd1257909c2bebfd77d889
Chris Boydstun <52934303+cboydstun@users.noreply.github.com>
2024-02-09 21:28:18 -0600

    added cors

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const { testConnection } = require('./db/conn');
 const { authenticateJWT } = require('./middlewares/authMiddleware');
+const cors = require('cors');
 
 const app = express();
 const PORT = 8080;
 
 testConnection();
 app.use(express.json());
+app.use(cors());
 
 app.get('/health', (req, res) => {
     res.send('OK');
