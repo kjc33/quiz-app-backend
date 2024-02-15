b91bb05ab4bbedb95470cdecae751c023446cb82
Kyle Chin <144987241+kjc33@users.noreply.github.com>
2024-02-14 23:20:09 -0800

    Added Constants.js

---
+++ models/constants.js
@@ -0,0 +1,13 @@
+const TABLE_NAMES = {
+    USERS: 'users',
+  };
+  
+  const COLUMN_NAMES = {
+    USERS: {
+      ID: 'id',
+      USER_NAME: 'user_name',
+      SCORE: 'score',
+    },
+  };
+  
+  module.exports = { TABLE_NAMES, COLUMN_NAMES };
