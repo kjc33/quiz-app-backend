c744d5a6926e2d04e2509c431baecd4cb6d0f051
Kyle Chin <144987241+kjc33@users.noreply.github.com>
2024-02-15 23:46:50 -0800

    Update UserRoutes Import

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const { testConnection } = require("./db/conn");
 const cors = require("cors");
 const { sequelize } = require("./db/conn");
-const UserRoutes = require("./routes/UserRoutes");
+const UserRoutes = require(__dirname + "/routes/UserRoutes");
 const QuestionRoutes = require("./routes/QuestionRoutes");
 const AdminRoutes = require("./routes/AdminRoutes");
 
